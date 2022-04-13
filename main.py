580805cca39797d7fa2f601e8abe870e41af0862
monteirolc <lucasm.castro47@gmail.com>
2022-04-13 11:16:28 -0300

    Feat.: Added sleep for big texts

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import gtts
 from playsound import playsound
+from time import sleep
 
 with open('frase.txt', 'r') as file:
     for line in file:
-        mySound = gtts.gTTS(line,lang='pt-br')
-        mySound.save('speak.mp3')
-        playsound('speak.mp3')
+        mySound = gtts.gTTS(line,lang='pt-BR')
+        mySound.save('talktome.mp3')
+        sleep(5)
+        playsound('talktome.mp3')
